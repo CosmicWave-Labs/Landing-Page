99f55d1eea57c9da0b7f149053cf1a489618a907
Scrabby <scrab_2877@hotmail.com>
2024-08-06 22:37:41 -0600

    React icons

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+
+const Footer:FC = () => {
+
+    return (
+        <section>
+            <div>
+                <h1> CONTACTANOS </h1>
+                <div> 
+                    BOX 1 MINI
+                </div> 
+                
+                <div>
+                    <h1>info</h1>
+                </div> 
+            </div>
+        </section>
+    )
+
+}
+
+export default Footer
